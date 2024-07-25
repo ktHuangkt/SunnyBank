a53ce39471b94d3cec13bf6e3a90443dc43c38c8
plokm5151 <plokm85222131@gmail.com>
2024-07-25 16:57:27 +0800

    feat: 富蘭克林資訊增加日期以及連結

--- web-scraping/franklin.py
+++ web-scraping/franklin.py
@@ -1,3 +1,4 @@
+import re
 import pygsheets
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -7,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
+import datetime
 
 
 
@@ -33,23 +35,24 @@ def extract_all_text(soup):
     texts = soup.stripped_strings
     return list(texts)
 
-def parse_text_to_title_content(text_list):
+def parse_text_to_title_content(text_list, url, current_date):
     sections = ['美國股市', '美國債市', '歐洲', '亞洲', '中港台', '拉美', '黃金']
     parsed_data = []
     current_title = None
     current_content = []
+    current_link = url
 
     for text in text_list:
         if text in sections:
             if current_title and current_content:
-                parsed_data.append((current_title, ' '.join(current_content)))
+                parsed_data.append((current_title, ' '.join(current_content), current_date, current_link))
             current_title = text
             current_content = []
         else:
             current_content.append(text)
     
     if current_title and current_content:
-        parsed_data.append((current_title, ' '.join(current_content)))
+        parsed_data.append((current_title, ' '.join(current_content), current_date, current_link))
     
     return parsed_data
 
@@ -58,11 +61,19 @@ def save_to_google_sheets(parsed_data, key, url):
         gc = pygsheets.authorize(service_file=key)
         sh = gc.open_by_url(url)
         wks = sh.worksheet_by_title("工作表1")
-        df = pd.DataFrame(parsed_data, columns=['Title', 'Content'])
+        df = pd.DataFrame(parsed_data, columns=['Title', 'Content', 'Date', 'Link'])
         wks.clear()
         wks.set_dataframe(df, (1, 1))
     except Exception as e:
         print(f"Error saving to Google Sheets: {e}")
+
+def get_today_date_from_soup(soup):
+    date_element = soup.find('date')
+    if date_element:
+        date_text = date_element.get_text(strip=True)
+        if date_text.startswith("日期："):
+            return date_text.replace("日期：", "").strip()
+    return datetime.datetime.today().strftime('%Y-%m-%d')  # 返回今天的日期
 
 def main():
     url = "https://www.franklin.com.tw/dailynews/Daily_A.html"
@@ -73,8 +84,9 @@ def main():
     try:
         soup = fetch_page(driver, url)
         if soup:
+            current_date = get_today_date_from_soup(soup)  # 提取日期
             all_texts = extract_all_text(soup)
-            parsed_data = parse_text_to_title_content(all_texts)
+            parsed_data = parse_text_to_title_content(all_texts, url, current_date)
             save_to_google_sheets(parsed_data, key, spreadsheet_url)
             print(parsed_data)
     finally:
